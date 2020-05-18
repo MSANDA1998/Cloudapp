47634fe28fe718985e9c4e71c34da6b4aef30627
MSANDA1998 <61823486+MSANDA1998@users.noreply.github.com>
2020-05-18 15:16:30 +0000

    Add files via upload

---
+++ ListeFichiers.js
@@ -0,0 +1,19 @@
+import React from 'react' 
+
+const ListeFichiers =({projects}) => {
+    return(
+        <div className="Liste-Fichiers section">
+            
+            <div className="card z-depth-0 project-summary">
+            <div className="card-content grey-text text-darken-3">
+            <span className="card-title">Bienvenue à votre application de stockage et de gestion des fichiers dans le Cloud. CloudAPP vous permet de télécharger des fichiers, si vous êtes déjà inscris, connectez-vous, sinon inscrivez-vous.</span>
+    
+            </div>
+            </div>
+        </div>    
+
+        
+        
+    )
+}
+export default ListeFichiers
